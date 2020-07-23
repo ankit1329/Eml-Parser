4b3152fc5dd3eb4289e54a96501c30a15b8fb7a2
ankit <ankit@discoverdollar.com>
2020-07-23 14:34:19 +0530

    initial commit

---
+++ lib.js
@@ -0,0 +1,163 @@
+const fs = require('fs');
+const simpleParser = require('mailparser').simpleParser;
+const pdf = require('html-pdf');
+
+module.exports = EmlParser = function (fileReadStream) {
+
+    this.parsedEmail;
+
+    this.parseEml = () => {
+        return new Promise((resolve, reject) => {
+            if (this.parsedEmail) {
+                resolve(this.parsedEmail);
+            } else {
+                simpleParser(fileReadStream, {})
+                    .then(result => {
+                        this.parsedEmail = result;
+                        resolve(this.parsedEmail);
+                    })
+                    .catch(err => {
+                        reject(err);
+                    });
+            }
+        })
+    }
+
+    this.getEmailHeaders = () => {
+        return new Promise((resolve, reject) => {
+            this.parseEml()
+                .then(result => {
+                    let headers = {
+                        subject: result.subject,
+                        from: result.from.value,
+                        to: result.to.value,
+                        cc: result.cc.value,
+                        date: result.date,
+                        inReplyTo: result.inReplyTo,
+                        messageId: result.messageId
+                    }
+                    resolve(headers)
+                })
+                .catch(err => {
+                    reject(err);
+                })
+        })
+    }
+
+
+    this.getEmailBodyHtml = () => {
+        let replacements = {
+            "’": "'",
+            "–": "&#9472;"
+        }
+        return new Promise((resolve, reject) => {
+            this.parseEml()
+                .then(result => {
+                    let htmlString = result.html;
+                    for (var key in replacements) {
+                        let re = new RegExp(key, 'gi')
+                        htmlString = htmlString.replace(re, replacements[key]);
+                    }
+                    resolve(htmlString);
+                })
+                .catch(err => {
+                    reject(err);
+                })
+
+        })
+    }
+
+    this.getEmailAsHtml = () => {
+        return new Promise((resolve, reject) => {
+            this.parseEml()
+                .then(result => {
+
+                    let headerHtml = `
+                    <div style="border:1px solid gray;margin-bottom:5px;padding:5px">
+                        <h2>${result.subject}</h2>
+                        <div style="display:flex;width:100%;">
+                            <span style="font-weight:600;">From:&nbsp;${result.from.html}</span>
+                            <span style="flex: 1 1 auto;"></span>
+                            <span style="color:silver;font-weight:600">${new Date(result.date).toLocaleString()}</span>
+                        </div>
+                        <div style="font-size:12px;">
+                            To:&nbsp;${result.to.html}
+                        </div>
+                        <div style="font-size:12px;">
+                            Cc:&nbsp;${result.cc.html}
+                        </div>
+                    </div>
+                    `
+                    this.getEmailBodyHtml()
+                        .then(bodyHtml => {
+                            resolve(headerHtml + `<p>${bodyHtml}</p>`)
+                        })
+                        .catch(err => {
+                            reject(err);
+                        })
+                })
+                .catch(err => {
+                    reject(err);
+                })
+
+        })
+    }
+
+    this.convertEmailToStream = (type) => {
+        return new Promise((resolve, reject) => {
+            let options = {
+                orientation: 'landscape'
+            };
+            if (type) {
+                options.type = type;
+            }
+            this.getEmailAsHtml()
+                .then(html => {
+                    pdf.create(html, options).toStream(function (err, res) {
+                        if (err) {
+                            reject(err);
+                        }
+                        resolve(res);
+                    });
+                })
+                .catch(err => {
+                    reject(err);
+                })
+        })
+    }
+
+    this.convertEmailToBuffer = (type) => {
+        return new Promise((resolve, reject) => {
+            let options = {
+                orientation: 'landscape'
+            };
+            if (type) {
+                options.type = type;
+            }
+            this.getEmailAsHtml()
+                .then(html => {
+                    pdf.create(html, options).toBuffer(function (err, res) {
+                        if (err) {
+                            reject(err);
+                        }
+                        resolve(res);
+                    });
+                })
+                .catch(err => {
+                    reject(err);
+                })
+        })
+    }
+
+    this.getEmailAttachments = () => {
+        return new Promise((resolve, reject) => {
+            this.parseEml()
+                .then(result => {
+                    resolve(result.attachments);
+                })
+                .catch(err => {
+                    reject(err);
+                })
+        })
+    }
+}
